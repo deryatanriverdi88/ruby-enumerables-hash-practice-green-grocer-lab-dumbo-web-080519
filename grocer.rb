3a35b544a45c5a9618d57f05dd9d7bd9b9bb9700
Derya Tanriverdi <deryatanriverdi_@hotmail.com>
2019-08-04 02:37:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,58 @@
+require "pry"
+
+foods =
+[
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"KALE"    => {:price => 3.00, :clearance => false}}
+]
+
 def consolidate_cart(cart)
-  # code here
+  result = {}
+  cart.each do |food_list|
+    food_list.each do |key, value|
+       
+      if result.key?(key)
+        result[key][:count] += 1
+        
+        
+      else
+        result[key] = value
+        result[key][:count] = 1
+      end
+    end
+    
+  end
+  result
+ 
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+p consolidate_cart(foods)
+
+def apply_coupons(cart,coupons)
+coupons.each do |coupon|
+    coupon_name = coupon[:item]
+    coupon_item_num = coupon[:num]
+    cart_item = cart[coupon_name]
+
+     next if cart_item.nil? || cart_item[:count] < coupon_item_num
+
+     cart_item[:count] -= coupon_item_num
+
+     coupon_in_cart = cart["#{coupon_name} W/COUPON"]
+
+     if coupon_in_cart
+      coupon_in_cart[:count] += 1
+    else
+      cart["#{coupon_name} W/COUPON"] = { 
+        price: coupon[:cost], 
+        clearance: cart_item[:clearance], 
+        count: 1
+      }
+    end
+  end
+
+   cart
 end
 
 def apply_clearance(cart)
